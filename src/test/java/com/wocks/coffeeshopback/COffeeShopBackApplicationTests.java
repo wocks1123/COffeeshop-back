417e4f3dbbc12e174d47df1fc37c3cf6f2fe4768
DevRyu10 <122430128+DevRyu10@users.noreply.github.com>
2023-11-03 04:20:12 +0900

    #1 프로젝트 생성
    - springboot 프로젝트 생성
    - 프로젝트 생성 및 의존성 라이브러리 설정

---
+++ src/test/java/com/wocks/coffeeshopback/COffeeShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wocks.coffeeshopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class COffeeShopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
